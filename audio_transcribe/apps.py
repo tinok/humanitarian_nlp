e2102c727e38df5725f907d91dfc3dc7255a1a22
tinok <tino.kreutzer@gmail.com>
2018-11-16 22:04:39 -0500

    creating django app with react

---
+++ audio_transcribe/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AudioTranscribeConfig(AppConfig):
+    name = 'audio_transcribe'
---
+++ build/precache-manifest.a965785263eb2d681b2c6ef9263f5ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac3ca2c5ad496290968e",
+    "url": "/static/js/main.ac3ca2c5.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "ac3ca2c5ad496290968e",
+    "url": "/static/css/main.50b39da2.chunk.css"
+  },
+  {
+    "revision": "83001a4c767b5793641de38eddd9559f",
+    "url": "/index.html"
+  }
+];
